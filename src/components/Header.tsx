2d1bd1b6c38a7c0d3902b8c4531d541279c91f03
Antonov Kirill <antonovkir2@gmail.com>
2022-03-01 13:27:23 +0500

    unit 14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://i.pinimg.com/originals/de/1f/9c/de1f9c87bdf6c5185a95557141bc4a0d.png" alt=""/>
         </header>
     );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
-            <div className="">
+        <div className={s.content}>
+            <div>
                 <img src="https://s1.1zoom.ru/b5050/297/Canada_Mountains_Scenery_488936_3840x2400.jpg" alt=""/>
             </div>
-            <div className="">
+            <div>
                 ava + descr
             </div>
-            <div className="">
-                my  posts
-                <div className="">
+            <div>
+                my posts
+                <div>
                     New post
                 </div>
-                <div className="">post 1</div>
-                <div className="">post 2</div>
-                <div className="">post 3</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
+                </div>
             </div>
         </div>
     );
