c74fe37dfeaf2dcda6c20dac52caa435446d8fc2
Antonov Kirill <antonovkir2@gmail.com>
2022-03-01 13:59:00 +0500

    unit 15

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from "./Post.module.css";
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://avatars.mds.yandex.net/get-zen_doc/3381150/pub_5fe595608380f70f48b64390_5fe59ece8380f70f48c5d6e3/scale_1200" alt=""/>
+            post 1
+        </div>
+
+    );
+};
+
+export default Post;
