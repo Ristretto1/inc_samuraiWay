e4eae663dd3555deb1a42f337e07e5e94b850976
Antonov Kirill <antonovkir2@gmail.com>
2022-03-01 11:46:29 +0500

    12 unit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://i.pinimg.com/originals/de/1f/9c/de1f9c87bdf6c5185a95557141bc4a0d.png" alt=""/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className="">
+                <img src="https://s1.1zoom.ru/b5050/297/Canada_Mountains_Scenery_488936_3840x2400.jpg" alt=""/>
+            </div>
+            <div className="">
+                ava + descr
+            </div>
+            <div className="">
+                my  posts
+                <div className="">
+                    New post
+                </div>
+                <div className="">post 1</div>
+                <div className="">post 2</div>
+                <div className="">post 3</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
