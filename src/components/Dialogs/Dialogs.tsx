57a6eb0d5c4c502ac86e1da11ea8559f62b5da31
Antonov Kirill <antonovkir2@gmail.com>
2022-03-04 10:19:18 +0500

    unit 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Anya
+                </div>
+                <div className={s.dialog}>
+                    Ivan
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messagesItems}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    HIHI
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     );
 };
