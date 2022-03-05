abc4d1f1bc08e3e80ba5edf9278b682bfa8c03ec
Antonov Kirill <antonovkir2@gmail.com>
2022-03-05 14:13:22 +0500

    unit 24

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,19 +6,44 @@ import Message from "./Message/Message";
 
 
 const Dialogs = () => {
+
+    type dialogsDataType = {
+        id: number,
+        name: string
+    }
+
+    let dialogsData: Array<dialogsDataType> = [
+        {id:1, name:'Kirill'},
+        {id:2, name:'Dima'},
+        {id:3, name:'Anya'},
+        {id:4, name:'Ivan'},
+        {id:5, name:'Sveta'},
+    ]
+
+    type messagesDataType = {
+        id: number,
+        message: string
+    }
+
+    let messagesData: Array<messagesDataType> = [
+        {id:1, message:'Hi'},
+        {id:2, message:'HIHI'},
+        {id:3, message:'Yo'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id={1} name={'Kirill'}/>
-                <DialogItem id={2} name={'Dima'}/>
-                <DialogItem id={3} name={'Anya'}/>
-                <DialogItem id={4} name={'Ivan'}/>
-                <DialogItem id={5} name={'Sveta'}/>
+                <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+                <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
+                <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
+                <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
+                <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}/>
             </div>
             <div className={s.messagesItems}>
-                <Message message={'Hi'}/>
-                <Message message={'HIHI'}/>
-                <Message message={'Yo'}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,18 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    type myPostsDataType = {
+        id: number,
+        message: string,
+        likes: number
+    }
+
+    let myPostsData: Array<myPostsDataType> = [
+        {id: 1, message: 'Hi, how are you?', likes: 15},
+        {id: 2, message: 'It is your first post', likes: 20}
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>my posts</h3>
@@ -16,8 +28,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post likes={15} message={'Hi, how are you?'}/>
-                <Post likes={20} message={'It is your first post'}/>
+                <Post likes={myPostsData[0].likes} message={myPostsData[0].message}/>
+                <Post likes={myPostsData[1].likes} message={myPostsData[1].message}/>
             </div>
         </div>
     );
