189189afb2e9b5aca58ed35ddd6b37a648c6b3e8
Antonov Kirill <antonovkir2@gmail.com>
2022-03-06 12:45:03 +0500

    update BLL

--- src/index.tsx
+++ src/index.tsx
@@ -7,40 +7,45 @@ export type dialogsType = {
     id: number,
     name: string
 }
-
-let dialogs: Array<dialogsType> = [
-    {id: 1, name: 'Kirill'},
-    {id: 2, name: 'Dima'},
-    {id: 3, name: 'Anya'},
-    {id: 4, name: 'Ivan'},
-    {id: 5, name: 'Sveta'},
-]
-
 export type messagesType = {
     id: number,
     message: string
 }
-
-let messages: Array<messagesType> = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'HIHI'},
-    {id: 3, message: 'Yo'},
-    {id: 3, message: 'YoYo'},
-]
-
 export type myPostsType = {
     id: number,
     message: string,
     likes: number
 }
 
-let myPosts: Array<myPostsType> = [
-    {id: 1, message: 'Hi, how are you?', likes: 15},
-    {id: 2, message: 'It is your first post', likes: 20},
-    {id: 2, message: 'It is your second post', likes: 30}
-]
+export type stateType = {
+    dialogs: Array<dialogsType>,
+    messages: Array<messagesType>,
+    myPosts: Array<myPostsType>,
+}
+
+let state: stateType = {
+    dialogs: [
+        {id: 1, name: 'Kirill'},
+        {id: 2, name: 'Dima'},
+        {id: 3, name: 'Anya'},
+        {id: 4, name: 'Ivan'},
+        {id: 5, name: 'Sveta'},
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'HIHI'},
+        {id: 3, message: 'Yo'},
+        {id: 3, message: 'YoYo'},
+    ],
+    myPosts: [
+        {id: 1, message: 'Hi, how are you?', likes: 15},
+        {id: 2, message: 'It is your first post', likes: 20},
+        {id: 2, message: 'It is your second post', likes: 30}
+    ]
+}
+
 
 ReactDOM.render(
-    <App messages={messages} dialogs={dialogs} myPosts={myPosts}/>,
+    <App messages={state.messages} dialogs={state.dialogs} myPosts={state.myPosts}/>,
     document.getElementById('root')
 );
